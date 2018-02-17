d644d198207df485f8f427d9254a7e1414eb058d
Noelle Brockhoff <noelle@nbrockhoff.com>
2018-02-17 00:14:40 -0600

    added gallery template with mustache

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,13 @@
 global.jQuery = require('jquery');
-popper = require('popper.js');
 bootstrap = require('bootstrap');
 mustache = require('mustache');
 
-jQuery(document).ready(function() {
-    alert('Bonjour, Bitches!');
+jQuery(document).ready(function($) {
+    var jqxhr = $.getJSON('data.json', function() {
+
+    }).done(function(data) {
+        var template = $('#template').html();
+        var showTemplate = mustache.render(template, data);
+        $('#gallery').html(showTemplate);
+    });
 });
